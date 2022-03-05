3606d75b50c7f50a4f6cc094edb2cd908c363315
rene ibarra <reidisy@gmail.com>
2022-03-04 18:23:05 -0600

    init maven project

--- customer/src/main/java/com/renemicroservices/customer/CustomerApplication.java
+++ customer/src/main/java/com/renemicroservices/customer/CustomerApplication.java
@@ -1,2 +1,17 @@
-package com.renemicroservices.customer;public class CustomerApplication {
+package com.renemicroservices.customer;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.context.annotation.PropertySource;
+        import org.springframework.context.annotation.PropertySources;
+@SpringBootApplication(
+        scanBasePackages = {
+
+        }
+)
+
+
+public class CustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
 }
